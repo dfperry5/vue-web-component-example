7d1f82befd467daf77bdb8d7f8ecf067c5cc1467
Dylan Perry <DylanPerry5@gmail.com>
2019-05-21 15:37:22 -0400

    Updating to include the latest

---
+++ vue-web-component/src/main.js
@@ -0,0 +1,10 @@
+import Vue from 'vue';
+import wrap from '@vue/web-component-wrapper';
+import VueWebComponent from './components/VueWebComponent';
+// import OtherWebComponent from './components/OtherWebComponent';
+
+const CustomElement = wrap(Vue, VueWebComponent);
+// const OtherElement = wrap(Vue, OtherWebComponent);
+
+window.customElements.define('my-custom-element', CustomElement);
+// window.customElements.define('my-other-element', OtherElement);
